c879abe035ebe1c5cc7d9fcbea6212dba658875a
rluisr <detteiu.x14th@gmail.com>
2015-11-13 19:06:11 +0900

    first

---
+++ delNginxCache.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: delNginxCache
+Plugin URI: https://luispc.com/
+Description: 記事投稿時にNginxのキャッシュを削除する - When you post new article,delete nginx cache.
+Author: luis
+Version: 0.1
+Author URI:http://luispc.com/
+*/
+ ?>
