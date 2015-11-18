5e058400d642945b5c5977b9ad85d86fd78b8f78
rluisr <detteiu.x14th@gmail.com>
2015-11-18 15:26:01 +0900

    Update delNginxCache.php

--- delNginxCache.php
+++ delNginxCache.php
@@ -7,4 +7,46 @@ Author: luis
 Version: 0.1
 Author URI:http://luispc.com/
 */
- ?>
+
+add_action('publish_post', 'delete_nginx_cache');
+add_action('admin_menu', 'admin_nginx_cache');
+
+function admin_nginx_cache()
+{
+    add_menu_page(
+        'test',
+        'test1',
+        'administrator',
+        'deleteNginxCache',
+        'menu_nginx_cache'
+    );
+}
+
+function menu_nginx_cache()
+{
+    if (isset($_POST['dir'])) {
+        update_option('dir_nginx_cache', $_POST['dir']);
+    }
+    $dir = get_option('dir_nginx_cache');
+    echo <<<EOD
+<div>
+    <h2>deleteNginxCache</h2>
+    <form action="" method="post">
+        <p><input type="text" name="dir" value="{$dir}" /></p>
+        <p><input type="submit" value="Save" /></p>
+    </form>
+</div>
+EOD;
+
+}
+
+function delete_nginx_cache()
+{
+    $dir = get_option('dir_nginx_cache');
+    $fileName = "${dir}/*";
+    foreach (glob($fileName) as $a) {
+        unlink($a);
+    }
+}
+
+?>
